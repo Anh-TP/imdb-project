6a269d8a86b96a29e39549b4c69eead992bf2212
Anh Phan <anh.phan@ten10.com>
2025-01-18 16:36:39 +0000

    Added initial spring dependencies and properties
    Added Spring dependencies for web & database to pom.xml
    Added initial properties for H2 database
    Added the movies application class

---
+++ src/test/java/com/apdev/imbd/ImbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apdev.imbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
